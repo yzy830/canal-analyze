6353b71a41410c00ae9546c1299c98f9e5196641
yang zongyuan <yangzongyuan@pxsj.com>
2017-06-01 10:10:21 +0800

    分析了EventTransactionBuffer的工作机制

--- parse/src/main/java/com/alibaba/otter/canal/parse/inbound/EventTransactionBuffer.java
+++ parse/src/main/java/com/alibaba/otter/canal/parse/inbound/EventTransactionBuffer.java
@@ -14,6 +14,16 @@ import com.alibaba.otter.canal.store.CanalStoreException;
 /**
  * 缓冲event队列，提供按事务刷新数据的机制
  * 
+ * <p>
+ * {@code EventTransactionBuffer}采用循环buffer设计。维护两个下标{@code putSequence}和{@code flushSequence}。
+ * {@code putSequence}表示最后一次写入buffer的位置，{@code flushSequence}表示最后一条被flush数据的位置。如果
+ * <code>putSequence > flushSequence</code>，则表示buffer已经写满，必须要强制flush。 
+ * </p>
+ * <p>
+ * {@code EventTransactionBuffer}的目的是，将一个事务内部的事件，打包在一起，发送给客户端；如果一个事务的大小，超过了bufferSize，那么
+ * 这个事务将会被拆分成多次发送。
+ * </p>
+ * 
  * @author jianghang 2012-12-6 上午11:05:12
  * @version 1.0.0
  */
@@ -62,6 +72,18 @@ public class EventTransactionBuffer extends AbstractCanalLifeCycle {
         }
     }
 
+    /**
+     * {@code add}方法，在遇到transactionbegin事件时，会flush之前的数据；在遇到transactionend事件时，会刷新buffer中
+     * 现有的数据。对于ROWDATA事件，只会缓存DELETE/UPDATE/INSERT三种dml事件，其他事件不缓存，直接刷新。
+     * 
+     * <p>
+     * 由于在autocommit=1时，所有的dml都会在一个默认事务中执行；而当autocommit=0时，只有当手动提交事务时，操作才会写入日志。因此，
+     * 两种情况下，都不会造成数据在buffer中，不被提交
+     * </p>
+     * 
+     * @param entry
+     * @throws InterruptedException
+     */
     public void add(CanalEntry.Entry entry) throws InterruptedException {
         switch (entry.getEntryType()) {
             case TRANSACTIONBEGIN:
@@ -132,6 +154,12 @@ public class EventTransactionBuffer extends AbstractCanalLifeCycle {
         }
     }
 
+    /**
+     * 采用未操作代替模运算，要求buffer size必须是2的幂次方
+     * 
+     * @param sequcnce
+     * @return
+     */
     private int getIndex(long sequcnce) {
         return (int) sequcnce & indexMask;
     }
